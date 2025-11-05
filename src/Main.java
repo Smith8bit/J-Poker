e02b7e7ade9df5e36830fa8fe6c58913096fce82
Smith8bit <thesmithdot@gmail.com>
2025-11-05 07:00:59 +0700

    Incomplete Human-made Logic

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Player> playerList = new ArrayList<>();
-        playerList.add(new Player("player_1", 1000));
-        playerList.add(new Player("player_2", 1000));
-        playerList.add(new Player("player_3", 1000));
 
-        Game game = new Game(playerList, 20);
     }
 }
