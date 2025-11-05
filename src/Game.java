2b0606b5dfbe814319a42a9ea03c9868f8932db6
Smith8bit <thesmithdot@gmail.com>
2025-11-05 13:55:39 +0700

    50%

--- src/Game.java
+++ src/Game.java
@@ -14,12 +14,13 @@ public class Game {
     private Street street;
     private int bigBlind;
     private int smallBlind;
+    private boolean isCheckable = true;
+    private int CHECK_COUNTER;
 
     private int currentBet;
     private Map<String, Integer> playerBets;
     private List<String> activePlayerIds;
     private int currentActorPos;
-    public boolean checkable = true;
 
     public Game(List<Player> initialPlayers, int bigBlind) {
         this.players = new HashMap<>();
@@ -33,6 +34,7 @@ public class Game {
         this.smallBlind = bigBlind / 2;
         this.deck = new Deck();
         this.board = new ArrayList<>();
+        this.CHECK_COUNTER = 0;
     }
 
     public void start() {
@@ -55,6 +57,7 @@ public class Game {
         this.pot += _postBet(sbPlayerId, this.smallBlind);
         this.pot += _postBet(bbPlayerId, this.bigBlind);
         this.currentBet = this.bigBlind;
+        this.playerBets.put(bbPlayerId, bigBlind);
 
         // DEAL
         deck.shuffle();
@@ -67,30 +70,78 @@ public class Game {
         this.currentActorPos = 2 % this.playerOrder.size();
     }
 
-    public void handleAction(String playerId, String action, int amount) {
-        switch (action) {
-            case "Fold" :
-                this.activePlayerIds.remove(playerId);
-                break;
-            case "Check":
-                this.currentActorPos = (this.currentActorPos + 1) % this.playerOrder.size();;
-                checkable = true;
-                break;
-            case "Bet" :
-                ;
-                checkable = false;
-                break;
-            case "Call" :
-                ;
-                checkable = false;
-                break;
-            case "Raise":
-                ;
-                checkable = false;
-                break;
-        }
+    public void Fold(String playerId) {
+        this.activePlayerIds.remove(playerId);
+        System.out.println("Player " + playerId + " folds.");
 
+        if (_isRoundOver()) {
+            _progress();
+        } else {
+            _rotatePlayer();
+        }
+    }
+
+    public void Check() {
+        if (this.isCheckable) {
+            this.CHECK_COUNTER++;
+
+            if (_isRoundOver()) {
+                _progress();
+            } else {
+                _rotatePlayer();
+            }
+
+        } else {
+            System.out.println("Can't Check. Must Call, Raise or Fold");
+        }
+    }
+
+    public void Bet(String playerId, int amount) {
+        this.isCheckable = false;
+        this.pot += _postBet(playerId, amount);
+        this.currentBet = this.playerBets.get(playerId); // New current bet
+        System.out.println("Player " + playerId + " bets " + amount);
+
+        if (_isRoundOver()) {
+            _progress();
+        } else {
+            _rotatePlayer();
+        }
+    }
+
+    public void Call(String playerId) {
+        this.isCheckable = false;
+        int callAmount = this.currentBet - this.playerBets.get(playerId);
+        this.pot += _postBet(playerId, callAmount);
+        System.out.println("Player " + playerId + " calls " + callAmount);
+        if (_isRoundOver()) {
+            _progress();
+        } else {
+            _rotatePlayer();
+        }
+    }
+
+    public void Raise(String playerId, int amount) {
+        this.isCheckable = false;
+        this.pot += _postBet(playerId, amount);
+        this.currentBet = this.playerBets.get(playerId); // New current bet
+        System.out.println("Player " + playerId + " raise to " + amount);
+        if (_isRoundOver()) {
+            _progress();
+        } else {
+            _rotatePlayer();
+        }
+    }
+
+    private void _rotatePlayer() {
         this.currentActorPos = (this.currentActorPos + 1) % this.playerOrder.size();
+    }
+
+    private boolean _isRoundOver() {
+        // true if all current have same bet
+        // true if bet is 0 and CHECK_COUNTER is equal to activePlayer size
+        // true when activePlayer size = 1
+        return false;
     }
 
     public void _progress() {
@@ -99,23 +150,22 @@ public class Game {
         this.street = Street.values()[this.street.ordinal()+1];
 
         this.currentBet = 0;
+        this.isCheckable = true;
+        this.CHECK_COUNTER = 0;
         for (String id : this.playerBets.keySet()) {
             this.playerBets.put(id, 0);
         }
 
         switch (this.street) {
             case FLOP:
-                this.deck.deal(); // Burn card
                 this.board = this.deck.deal(3);
                 System.out.println("--- FLOP ---: " + this.board);
                 break;
             case TURN:
-                this.deck.deal(); // Burn card
                 this.board.add(this.deck.deal());
                 System.out.println("--- TURN ---: " + this.board);
                 break;
             case RIVER:
-                this.deck.deal(); // Burn card
                 this.board.add(this.deck.deal());
                 System.out.println("--- RIVER ---: " + this.board);
                 break;
@@ -149,7 +199,4 @@ public class Game {
 
     }
 
-    private void _evaluateHand() {
-
-    }
 }
