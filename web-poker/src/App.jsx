cdfff9697545fcc2091c6c6eed37c9c6951e1ae3
KengKaK <suppadej3@gmail.com>
2025-11-12 19:34:45 +0700

    Homepage

--- web-poker/src/App.jsx
+++ web-poker/src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PlayerList from './PlayerList/PlayerList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>COM SCI<br></br>POKER</h1>
+        <form>
+          <input type="text" id="Username" placeholder='ENTER YOUR NAME'/>
+          <br></br>
+          <button><h2>PLAY</h2></button>
+        </form>
+      {/* <PlayerList/> */}
     </>
   )
 }
