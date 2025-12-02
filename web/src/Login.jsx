ad4136ed2352f79eec3d6db552545a26efd2ca92
KengKaK <suppadej3@gmail.com>
2025-12-03 06:22:24 +0700

    update_css_login

--- web/src/Login.jsx
+++ web/src/Login.jsx
@@ -33,22 +33,24 @@ function Login() {
   
   return (
     <>
-    <h1 className="gametitle">COM SCI<br/>POKER</h1>
-        <form onSubmit={handleSubmit}>
-            <input className="strInput" 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Enter username"
-                required
-                disabled={loading}
-            />
-            <br></br>
-            <button type="submit" disabled={loading}>
-                {loading ? 'Joining...' : 'PLAY'}
-            </button>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-        </form>
+    <div className="center-layout">
+      <h1 className="gametitle">COM SCI<br/>POKER</h1>
+          <form onSubmit={handleSubmit}>
+              <input className="strInput" 
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Enter username"
+                  required
+                  disabled={loading}
+              />
+              <br></br>
+              <button type="submit" disabled={loading}>
+                  {loading ? 'Joining...' : 'PLAY'}
+              </button>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+          </form>
+      </div>
     </>
   )
 }
