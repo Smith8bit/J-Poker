b11613d3e01a1714f644fc6d6e2bf1c2a897130e
KengKaK <suppadej3@gmail.com>
2025-12-04 00:25:10 +0700

    Add isHost function

--- engine/src/main/java/dev/gamfactory/poker/Player.java
+++ engine/src/main/java/dev/gamfactory/poker/Player.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 public class Player {
     String id;
+    String username;
     int stack;
+    boolean isHost;
     List<Card> hand;
 
-    public Player(String id, int stack) {
+    public Player(String id, String username, int stack) {
         this.id = id;
+        this.username = username;
         this.stack = stack;
+        this.isHost = false;
         this.hand = new ArrayList<>();
     }
+    public boolean isHost() { return isHost; }
+    public void setHost(boolean host) { isHost = host; }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setStack(int stack) {
+        this.stack = stack;
+    }   
+    
+    public String getUsername() { return username; }
+    public String getId() { return id; }
+    public int getStack() { return stack; }
 
     @Override
     public String toString() {
